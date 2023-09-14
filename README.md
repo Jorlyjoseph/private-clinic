ad11438410dd5c10a77edb2a41965ea12ce8dc0f
Jorlyjoseph <141339657+Jorlyjoseph@users.noreply.github.com>
2023-09-14 19:16:11 +0200

    stylecss

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# private-clinic
+## private-clinic
 private clinic dr app
+
+
+#Hydroclin
+collecting datas!
+
+##Description
+
+This is the application for Doctors. They can collect patients datas, maintain digital patient records. This enables them to easily access patient history, diagnosis, treatment plans, and test results, leading to better-informed decisions and improved patient care.
+
+##Main Functionalities
+
+User(Doctor)fill registration form
+User(Doctor)can log in the applications.
+doctors can fill registration forms to patients.
+search the Patients details.
+adding the patient history,
+Edit and update the registrations form.
+it can remove the data for patients
+it can be stored patients history include date and time.
+
+
+##Technologies Used:
+
+CSS
+JavaScript
+node Js
+npm package
+Express js
+mongoose
+mongodb Atles
+MongoDB Compass
+
+#Express.js:
+Middleware
+Routing
+Handlebars
+bcryptjs
+cookie-parser
+dotenv
+express
+express-session
+node_modules
+
+findMyId;
+
+findByIdAndUpdate;
+
+findByIdAndDelete
+
+findOne
+
+
+##Extra Links
+Trello
+Link: https://trello.com/invite/b/Uymy99mX/ATTI169b23dc25afc3fe246aba98cf68a3911CDE1714/private-clinic
+
+##Slides
+Link: 
+
+##Deploy
+Link : https://private-clinic.adaptable.app/
--- routes/diagnosis.routes.js
+++ routes/diagnosis.routes.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Patient = require('../models/Patient.model.js');
 
+// GET route for the diagnosis
+router.get('/patient/:id/diagnose', (request, response, next) => {
+  if (!request.session.currentUser) {
+    response.redirect('/user/login');
+    return;
+  }
+
+  const { id } = request.params;
+  Patient.findById(id).then((data) => {
+    response.render('patient/diagnose', { data: data });
+  });
+});
 
 // GET route for all diagnoses
 router.get('/patient/:id/diagnoses', (request, response, next) => {
@@ -13,14 +25,7 @@ router.get('/patient/:id/diagnoses', (request, response, next) => {
   const { id } = request.params;
   Patient.findById(id)
     .then((patient) => {
-  
-      const history = patient.diagnosisHistory.map(({ date, diagnosis }) => ({
-        date: `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`,
-        diagnosis
-      }));
-      response.render('patient/diagnoses-list', {
-        patient: { ...patient, diagnosisHistory: history }
-      });
+      response.render('patient/diagnoses-list', { patient: patient });
     })
     .catch((error) => {
       console.error(error);
@@ -28,43 +33,32 @@ router.get('/patient/:id/diagnoses', (request, response, next) => {
     });
 });
 
-
-
-// GET route for the diagnosis
-router.get('/patient/:id/diagnose', (request, response, next) => {
-    console.log('Handling /patient/:id/diagnose request');
-    const { id } = request.params;
-    response.redirect(`/patient/${id}/diagnoses`);
-  });
-
 // POST route for adding a new diagnosis
 router.post('/patient/:id/diagnose', (req, res, next) => {
-    const { id } = req.params;
-    const { diagnosisDate, diagnosis } = req.body;
-  
-    const newDiagnosis = {
-      date: diagnosisDate,
-      diagnosis: diagnosis
-    };
-  
-    Patient.findByIdAndUpdate(
-      id,
-      { $push: { diagnosisHistory: newDiagnosis } },
-      { new: true }
-    )
-      .then((updatedPatient) => {
-        if (!updatedPatient) {
-          res.status(404).send('Patient not found');
-          return;
-        }
-  
-        res.redirect(`/patient/${id}/diagnoses`);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      });
-  });
+  const { id } = req.params;
+  const { diagnosisDate, diagnosis } = req.body;
 
+  const newDiagnosis = {
+    date: diagnosisDate,
+    diagnosis: diagnosis
+  };
+
+  Patient.findByIdAndUpdate(
+    id,
+    { $push: { diagnosisHistory: newDiagnosis } },
+    { new: true }
+  )
+    .then((updatedPatient) => {
+      if (!updatedPatient) {
+        res.status(404).send('Patient not found');
+        return;
+      }
+
+      res.redirect(`/patient/${id}/diagnoses`);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    });
+});
 module.exports = router;
-

4e9805f4ec9be9404f18765c2d18c65895c5478c
linenogueira <134888996+linenogueira@users.noreply.github.com>
2023-09-14 19:24:03 +0200

    Update diagnosis.routes.js

--- routes/diagnosis.routes.js
+++ routes/diagnosis.routes.js
@@ -4,16 +4,10 @@ const Patient = require('../models/Patient.model.js');
 
 // GET route for the diagnosis
 router.get('/patient/:id/diagnose', (request, response, next) => {
-  if (!request.session.currentUser) {
-    response.redirect('/user/login');
-    return;
-  }
-
-  const { id } = request.params;
-  Patient.findById(id).then((data) => {
-    response.render('patient/diagnose', { data: data });
+    console.log('Handling /patient/:id/diagnose request');
+    const { id } = request.params;
+    response.redirect(`/patient/${id}/diagnoses`);
   });
-});
 
 // GET route for all diagnoses
 router.get('/patient/:id/diagnoses', (request, response, next) => {
@@ -33,32 +27,36 @@ router.get('/patient/:id/diagnoses', (request, response, next) => {
     });
 });
 
+
+
+
 // POST route for adding a new diagnosis
 router.post('/patient/:id/diagnose', (req, res, next) => {
-  const { id } = req.params;
-  const { diagnosisDate, diagnosis } = req.body;
-
-  const newDiagnosis = {
-    date: diagnosisDate,
-    diagnosis: diagnosis
-  };
-
-  Patient.findByIdAndUpdate(
-    id,
-    { $push: { diagnosisHistory: newDiagnosis } },
-    { new: true }
-  )
-    .then((updatedPatient) => {
-      if (!updatedPatient) {
-        res.status(404).send('Patient not found');
-        return;
-      }
-
-      res.redirect(`/patient/${id}/diagnoses`);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    });
-});
+    const { id } = req.params;
+    const { diagnosisDate, diagnosis } = req.body;
+  
+    const newDiagnosis = {
+      date: diagnosisDate,
+      diagnosis: diagnosis
+    };
+  
+    Patient.findByIdAndUpdate(
+      id,
+      { $push: { diagnosisHistory: newDiagnosis } },
+      { new: true }
+    )
+      .then((updatedPatient) => {
+        if (!updatedPatient) {
+          res.status(404).send('Patient not found');
+          return;
+        }
+  
+   
+        res.redirect(`/patient/${id}/diagnoses`);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      });
+  });
 module.exports = router;
